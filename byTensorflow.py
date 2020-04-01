142dd5f84c5a04b7378e8873d868fb144585c581
WeOneTeam <hoanhai0405@gmail.com>
2020-04-02 00:20:28 +0700

    done tensorflow!

---
+++ byTensorflow.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+train_images = train_images.reshape(train_images.shape[0],28,28,1)
+test_images = test_images.reshape(test_images.shape[0],28,28,1)
+
+train_labels = keras.utils.to_categorical(train_labels)
+test_labels = keras.utils.to_categorical(test_labels)
+
+train_images = train_images.astype('float32') / 255.0
+
+test_images = test_images.astype('float32') / 255.0
+
+model = keras.Sequential([
+    keras.layers.Conv2D(32, (3, 3), activation='relu',input_shape=(28, 28, 1)),
+    keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    keras.layers.Flatten(),
+    keras.layers.Dense(100, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
